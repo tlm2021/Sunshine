67acd326038c4da1168e05786fe982b9956d4c3c
Travis Mosley <travis@travismosley.com>
2014-09-30 00:13:49 -0700

    Finished writing testing Suite, fixed DB Helper

--- app/src/androidTest/java/com/example/travis/sunshine/app/test/TestDb.java
+++ app/src/androidTest/java/com/example/travis/sunshine/app/test/TestDb.java
@@ -7,7 +7,11 @@ import android.test.AndroidTestCase;
 import android.util.Log;
 
 import com.example.travis.sunshine.app.data.LocationContract.LocationEntry;
+import com.example.travis.sunshine.app.data.WeatherContract.WeatherEntry;
 import com.example.travis.sunshine.app.data.WeatherDbHelper;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by travis on 9/8/14.
@@ -15,6 +19,19 @@ import com.example.travis.sunshine.app.data.WeatherDbHelper;
 public class TestDb extends AndroidTestCase {
 
     public static final String LOG_TAG = TestDb.class.getSimpleName();
+
+    static public void validateCursor(ContentValues values, Cursor cursor) {
+        assertTrue(cursor.moveToFirst());
+
+        Set<Map.Entry<String, Object>> valueSet = values.valueSet();
+        for (Map.Entry<String, Object> entry : valueSet) {
+            String columnName = entry.getKey();
+            int idx = cursor.getColumnIndex(columnName);
+            assertFalse(idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals(expectedValue, cursor.getString(idx));
+        }
+    }
 
     public void testCreateDb() throws Throwable {
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
@@ -24,20 +41,35 @@ public class TestDb extends AndroidTestCase {
         db.close();
     }
 
+    ContentValues getLocationContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_LOC_NAME, "North Pole");
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, "99705");
+        values.put(LocationEntry.COLUMN_LONGITUDE, 64.772);
+        values.put(LocationEntry.COLUMN_LATITUDE, -147.355);
+        return values;
+    }
+
+    ContentValues getWeatherContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(WeatherEntry.COLUMN_DATETEXT, "20141205");
+        values.put(WeatherEntry.COLUMN_DEGREES, 1.1);
+        values.put(WeatherEntry.COLUMN_HUMIDITY, 1.2);
+        values.put(WeatherEntry.COLUMN_PRESSURE, 1.3);
+        values.put(WeatherEntry.COLUMN_MAX_TEMP, 75);
+        values.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
+        values.put(WeatherEntry.COLUMN_SHORT_DESC, "Asteroids");
+        values.put(WeatherEntry.COLUMN_WIND_SPEED, 5.5);
+        values.put(WeatherEntry.COLUMN_WEATHER_ID, 321);
+        return values;
+    }
+
     public void testInsertReadDb() {
-        String testName = "North Pole";
-        String testLocationSetting = "99705";
-        double testLatitude = 64.772;
-        double testLongitude = -147.355;
 
         WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(LocationEntry.COLUMN_LOC_NAME, testName);
-        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
-        values.put(LocationEntry.COLUMN_LONGITUDE, testLatitude);
-        values.put(LocationEntry.COLUMN_LATITUDE, testLongitude);
+        ContentValues values = getLocationContentValues();
 
         long locationRowId;
         locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
@@ -45,17 +77,9 @@ public class TestDb extends AndroidTestCase {
         assertTrue(locationRowId != -1);
         Log.d(LOG_TAG, "New row id: " + locationRowId);
 
-        String[] columns = {
-                LocationEntry._ID,
-                LocationEntry.COLUMN_LOCATION_SETTING,
-                LocationEntry.COLUMN_LOC_NAME,
-                LocationEntry.COLUMN_LONGITUDE,
-                LocationEntry.COLUMN_LATITUDE
-        };
-
         Cursor cursor = db.query(
                 LocationEntry.TABLE_NAME,
-                columns,
+                null,
                 null,
                 null,
                 null,
@@ -63,16 +87,31 @@ public class TestDb extends AndroidTestCase {
                 null
         );
 
-        if (cursor.moveToFirst()) {
-            int locationIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOCATION_SETTING);
-            String location = cursor.getString(locationIndex);
+        validateCursor(values, cursor);
 
-            int nameIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOC_NAME);
-            String name = cursor.getString(nameIndex);
+        // Fantastic.  Now that we have a location, add some weather!
 
-            int latIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LATITUDE);
+        ContentValues weatherValues = getWeatherContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
 
-        }
+        long weatherRowId;
+        weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherValues);
+
+        assertTrue(weatherRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + weatherRowId);
+
+        cursor = db.query(WeatherEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        validateCursor(weatherValues, cursor);
+
+        dbHelper.close();
 
     }
 }
--- app/src/main/java/com/example/travis/sunshine/app/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/travis/sunshine/app/data/WeatherDbHelper.java
@@ -24,22 +24,22 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         // Create a table to hold locations.
 
         final String SQL_CREATE_LOCATION_TABLE =
-                "CREATE TABLE " + LocationEntry.TABLE_NAME + "(" +
-                        LocationEntry._ID + " INTEGER PRIMARY KEY," +
+                "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                        LocationEntry._ID + " INTEGER PRIMARY KEY, " +
 
                         LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
                         LocationEntry.COLUMN_LOC_NAME + " TEXT NOT NULL, " +
 
                         LocationEntry.COLUMN_LATITUDE + " REAL NOT NULL, " +
-                        LocationEntry.COLUMN_LONGITUDE + " REAL NOT NULL " +
+                        LocationEntry.COLUMN_LONGITUDE + " REAL NOT NULL, " +
                         "UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE" +
                         " );";
 
         final String SQL_CREATE_WEATHER_TABLE =
                 "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
-                        WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 
-                        WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL," +
+                        WeatherEntry.COLUMN_LOC_KEY + " INTEGER NOT NULL, " +
                         WeatherEntry.COLUMN_DATETEXT + " TEXT NOT NULL, " +
                         WeatherEntry.COLUMN_SHORT_DESC + " TEXT NOT NULL, " +
                         WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, " +
